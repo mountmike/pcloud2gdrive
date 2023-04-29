4ab01f72c5e0cef1fc3088ffae5c40f56579853e
Michael <tharratt.michael@gmail.com>
2023-04-29 10:20:43 +1000

    reseting oAuth client details after leaking secrets

--- server/controllers/pcloud_controller.js
+++ server/controllers/pcloud_controller.js
@@ -1,21 +1,17 @@
 const config = require("../config")
 const express = require("express");
 const router = express.Router();
-const axios = require("axios")
 const Pcloud = require("../models/pcloud_model.js")
 
-
-router.get("/authURL", (req, res, next) => {
-    res.redirect(`https://my.pcloud.com/oauth2/authorize?redirect_uri=${config.pCloudAPI.redirectURI}&client_id=${config.pCloudAPI.clientId}&response_type=code`)
-})
 
 router.get("/auth-token", (req, res, next) => {
   const { code } = req.query
   Pcloud.getToken(code)
-  .then(resp => {
-    req.session.pCloud = resp
+  .then(token => {
+    req.session.pCloud = token
     res.send("<script>window.close();</script > ")
   })
+  .catch(next)
 });
 
 router.get('/folder/:folderId', async (req, res, next) => {
